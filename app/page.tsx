15f57242d7b2ecd27bcbcfb7fcf80e24bfa61f52
legendonnaib <160251630+legendonnaib@users.noreply.github.com>
2024-10-12 20:52:18 +0500

    done1

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer"
 import Hero from "./HeroSection/Hero";
 
-export default function () {
+function Homepage() {
   return (<div>
 
             <Header/>
@@ -13,3 +13,5 @@ export default function () {
     
   );
 }
+
+export default Homepage;
